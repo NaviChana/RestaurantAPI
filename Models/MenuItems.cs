2dfdf60dd1dbc2e8595e302121a2299cd7df1e0c
Navi Chana <NaviChana@outlook.com>
2022-10-02 13:59:58 -0600

    Add project files.

---
+++ Models/MenuItems.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantAPI2.Models
+{
+    public class MenuItems
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        public int? Price { get; set; }
+    }
+}
---
+++ Models/MenuItemsContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RestaurantAPI2.Models
+{
+    public class MenuItemsContext : DbContext
+    {
+        public MenuItemsContext(DbContextOptions<MenuItemsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MenuItems> MenuItems { get; set; }
+        
+    }
+}
